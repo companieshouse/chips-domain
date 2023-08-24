226c4e82b7cf4dbf448158f0e7005ed6685fddf0
Jake Metherell <jake@fluffylogic.com>
2023-08-24 14:29:02 +0100

    Add explanation to saml IA export script

--- container-scripts/export-saml2-as-ldif.py
+++ container-scripts/export-saml2-as-ldif.py
@@ -1,3 +1,14 @@
+# This script is intended to be run manually, after IDP metadata has been manually imported
+# into the Weblogic security realm.  The script exports all data, in LDIF format, 
+# from the SAML2 Identity Assertion Provider named "SAML_IA".
+# The output will be placed in /var/tmp/SAML_IA.ldif
+# That LDIF file can then be used to provide initial realm data when the domain is started, by
+# adding the data to a file bind mounted into the wladmin container as 
+# /apps/oracle/chipsdomain/security/SAML2IdentityAsserterInit.ldift
+#
+# The script can be run inside the wladmin container, as the weblogic user, with:
+# ${ORACLE_HOME}/oracle_common/common/bin/wlst.sh -skipWLSModuleScanning ${ORACLE_HOME}/container-scripts/export-saml2-as-ldif.py
+
 # Load environment variables
 domain_name = os.environ.get("DOMAIN_NAME", "wldomain")
 admin_name = os.environ.get("ADMIN_NAME", "wladmin")
